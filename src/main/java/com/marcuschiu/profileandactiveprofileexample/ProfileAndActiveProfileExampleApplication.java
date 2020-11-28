2fe10253f121171fc9fef29416e85489deed37d5
Marcus Chiu <marcuschiu@marcuss-mbp-2.lan>
2020-11-27 19:02:22 -0600

    initial commit

---
+++ src/main/java/com/marcuschiu/profileandactiveprofileexample/ProfileAndActiveProfileExampleApplication.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.profileandactiveprofileexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileAndActiveProfileExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileAndActiveProfileExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcuschiu/profileandactiveprofileexample/ProfileAndActiveProfileExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.profileandactiveprofileexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileAndActiveProfileExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
